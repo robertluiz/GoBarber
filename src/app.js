223ccc6f0f5b9baf68b30c58e7773814b8a147e9
Robert Luiz <robert.tiweb@gmail.com>
2019-10-21 10:51:53 -0300

    lint closes #3 #4 #5

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -9,6 +9,7 @@ class App {
   middlewares() {
     this.server.use(express.json());
   }
+
   routes() {
     this.server.use(routes);
   }
